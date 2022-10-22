84d7e202e4db9ce3fff535e6638266b605585071
Ольга Цинкевич <olyazink@jmail.com>
2022-10-22 20:33:51 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        // вывод полученных заголовков
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        List<Post> posts = mapper.readValue(
+                response.getEntity().getContent(), new
+                        TypeReference<List<Post>>() {
+                        });
+
+        posts.forEach(System.out::println);
+
+        response.close();
+        httpClient.close();
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,38 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+
+
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upvotes;
+
+    public Post(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }   // ... все getters
+
+    @Override
+    public String toString() {
+        return "Post" +
+                "\n  id = " + id +
+                "\n  text = " + text +
+                "\n  type = " + type +
+                "\n  user = " + user +
+                "\n  upvotes = " + upvotes;
+    }
+}
